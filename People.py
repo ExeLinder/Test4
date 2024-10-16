7e89bad6f398f23d3e72ff5524396f27cff73810
Sergey Ivanov <mailto.sergeyivanov@gmail.com>
2024-10-16 19:49:13 +0300

    refact code

--- People.py
+++ People.py
@@ -3,14 +3,14 @@ from Person import Person
 
 class People:
     def __init__(self):
-        self.people = []
+        self.__people = []
 
-    def add_person(self, person:Person):
-        self.people.append(person)
+    def add_person(self, person: Person):
+        self.__people.append(person)
 
-    def remove_person(self, person:Person):
-        if person in self.people:
-            self.people.remove(person)
+    def remove_person(self, person: Person):
+        if person in self.__people:
+            self.__people.remove(person)
         else:
             print("Person not found in list")
 
